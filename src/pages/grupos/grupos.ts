3d664b4c1721bccdcdc99a5a287108da50c6b52e
Wendell Clive Santos de Lira <wendell.clive@gmail.com>
2018-10-20 23:18:05 -0300

    listando categorias no App

--- src/pages/grupos/grupos.ts
+++ src/pages/grupos/grupos.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { GrupoService } from "../../services/domain/grupo.service";
+import { GrupoDTO } from "../../models/grupo.dto";
 
 /**
  * Generated class for the GruposPage page.
@@ -15,6 +16,9 @@ import { GrupoService } from "../../services/domain/grupo.service";
   templateUrl: "grupos.html"
 })
 export class GruposPage {
+
+  items: GrupoDTO[];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -24,7 +28,7 @@ export class GruposPage {
   ionViewDidLoad() {
     this.grupoService.findAll()
         .subscribe(response => {
-        console.log(response);
+        this.items = response;
     },
     error => {
       console.log(error);
